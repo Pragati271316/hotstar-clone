ba7dba2cf9ae29abe1f62b29d1190d1b8278e44e
Pragati271316 <pragatikhandagale7@gmail.com>
2025-03-14 18:04:44 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const watchNowButton = document.querySelector('.watch-now');
+
+watchNowButton.addEventListener('click', () => {
+    alert('Redirecting to watch page (not implemented in this example).');
+});
